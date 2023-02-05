9994d55e1460feabface36122f7f4e4505189153
R4FSAN-143 <124475382+R4FSAN-143@users.noreply.github.com>
2023-02-05 10:29:37 +0600

    Add files via upload

---
+++ sex_pk-bd.py
@@ -0,0 +1,55 @@
+import os, platform
+
+ 
+
+try:
+
+ 
+
+        import requests
+
+ 
+
+except:
+
+ 
+
+        os.system('pip2 install requests')
+
+ 
+
+ 
+
+ 
+
+bit = platform.architecture()[0]
+
+ 
+
+if bit == "64bit":
+
+        os.system('xdg-open https://www.facebook.com/groups/3206414299669908/?ref=share')
+
+ 
+
+        from sex import dijital
+
+ 
+
+        o()
+
+ 
+
+ 
+
+ 
+
+elif bit == "32bit":
+
+ 
+
+        os.system('xdg-open https://www.facebook.com/groups/3206414299669908/?ref=share')
+
+ 
+
+        os.system('python sex.py')
